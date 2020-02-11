26b220f8acb4a7b996708d76109aa851752c9c7a
william <william.nagels@gmail.com>
2020-02-12 00:36:56 +0100

    Turn out actionlib server spins own thread to execute goal callbacks. implement broken implementation with promise and future to get data from map callback context to actionlib context. if actionlib context doesnt' handle the update fast enough, promise will be satisfied twice which is trouble. Probably should simply mutex lock the occupancy grid shared ptr. Map context can't change as long as actionlib holds lock (plotting route). When actionlib releases it sleeps until condition_variable is set by OnMap context

--- src/server.cpp
+++ src/server.cpp
@@ -1,8 +1,8 @@
+#include <future>
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <vacuumcleaner/cleaningAction.h>
 #include "nav_msgs/OccupancyGrid.h"
-
 class CleaningAction
 {
 private:
@@ -17,56 +17,14 @@ private:
   nav_msgs::OccupancyGrid::ConstPtr _map;
   vacuumcleaner::cleaningGoalConstPtr _goal;
 
-  enum class State
-  {
-    WaitingForMap,
-    WaitingForGoal,
-    Navigating
-  };
-
-  void PlanNewRouteIfRequired()
-  {
-    if(true)
-    {
-      PlanRoute();
-    }
-  }
-  void PlanRoute()
+  bool _handled_map_update = true;
+  std::promise<nav_msgs::OccupancyGrid::ConstPtr> _map_promise;
+  bool PlanRoute()
   {
     ROS_INFO_STREAM("Launching mission");
     ROS_INFO_STREAM("map header: "<< _map->info);
+    return false;
   }
-  void GotMap()
-  {
-    if (_state == State::WaitingForMap)
-    {
-    	if (not _goal)
-	{
-	  _state = State::WaitingForGoal;
-	}
-	else
-	{
-	  PlanRoute();
-	}
-    }
-    else if (_state == State::Navigating)
-    {
-      PlanNewRouteIfRequired();
-    }
-  }
-  void GotGoal()
-  {
-    if (_state == State::WaitingForGoal)
-    {
-      PlanRoute();
-    }
-    else    
-    {
-      //errr? send help
-    }
- 
-  }
-  State _state = State::WaitingForMap;
 public:
 
   CleaningAction(std::string const& name) :
@@ -84,62 +42,26 @@ public:
     _action_server.start();
   }
 
-  ~CleaningAction(void)
-  {
+  void OnMap(nav_msgs::OccupancyGrid::ConstPtr const& map)
+  { 
+    ROS_INFO("%s: OnMap callback", _action_name.c_str());
+    _map_promise.set_value(map);
   }
 
-  void TryLaunch()
+  bool PlanRoute(nav_msgs::OccupancyGrid::ConstPtr map)
   {
-    if (not _map or not _goal)
-    {
-      return;   
-    }
+//   _map_promise = std::promise<nav_msgs::OccupancyGrid::ConstPtr>();
+//    ROS_INFO("%s: Planning route", _action_name.c_str());
+//    vacuumcleaner::cleaningResult result;
+//    result.number_of_tiles_visited = 10;
+//    _action_server.setSucceeded(result);
+    return true;
   }
-  void OnMap(nav_msgs::OccupancyGrid::ConstPtr const& map)
-  {
-    _map = map;
-    GotMap();
-  }
+  // This runs on simple actionlib thread
   void OnGoal(vacuumcleaner::cleaningGoalConstPtr goal)
   {
     _goal = goal;
-    GotGoal();
-//    ros::Rate r(1);
-//    bool success = true;
-//
-//    uint64_t number_of_tiles_visited = 0;
-//    uint64_t number_of_tiles_to_visit = 100;
-//    ROS_INFO_STREAM("Received goal: " << *goal);
-//
-//
-//    while(number_of_tiles_to_visit)
-//    {
-//      number_of_tiles_visited++;
-//      number_of_tiles_to_visit--;
-//
-//      // check that preempt has not been requested by the client
-//      if (_action_server.isPreemptRequested() || !ros::ok())
-//      {
-//        ROS_INFO("%s: Preempted", _action_name.c_str());
-//        // set the action state to preempted
-//        _action_server.setPreempted();
-//        success = false;
-//        break;
-//      }
-//      _feedback.number_of_tiles_visited = number_of_tiles_visited;
-//      _feedback.number_of_tiles_to_visit = number_of_tiles_to_visit;
-//      _action_server.publishFeedback(_feedback);
-//      r.sleep();
-//       
-//    }
-//
-//    if(success)
-//    {
-//      ROS_INFO("%s: Succeeded", _action_name.c_str());
-//      vacuumcleaner::cleaningResult result;
-//      result.number_of_tiles_visited = number_of_tiles_visited;
-//      _action_server.setSucceeded(result);
-//    }
+    do {} while(true);//PlanRoute(_map_promise.get_future().get()));
   }
 };
 
