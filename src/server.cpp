ae7b21e45d3f1f7b5debad05c02b4b82459698c9
william <william.nagels@gmail.com>
2020-02-13 00:10:13 +0100

    Fix threading issues on map update

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
-#include <future>
+#include <mutex>
+#include <condition_variable>
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <vacuumcleaner/cleaningAction.h>
@@ -7,24 +8,39 @@ class CleaningAction
 {
 private:
   ros::NodeHandle _node_handle;
-  actionlib::SimpleActionServer<vacuumcleaner::cleaningAction> _action_server; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+  actionlib::SimpleActionServer<vacuumcleaner::cleaningAction> _action_server;
   std::string _action_name;
-
-  // create messages that are used to published feedback/result
   vacuumcleaner::cleaningFeedback _feedback;
-  ros::Subscriber _map_subscriber;  
+  ros::Subscriber _map_subscriber; ///< Will subscribe to gmappping map updates.
+  vacuumcleaner::cleaningGoalConstPtr _goal; ///< received goal from actionlib client
 
-  nav_msgs::OccupancyGrid::ConstPtr _map;
-  vacuumcleaner::cleaningGoalConstPtr _goal;
-
-  bool _handled_map_update = true;
-  std::promise<nav_msgs::OccupancyGrid::ConstPtr> _map_promise;
-  bool PlanRoute()
+  struct ThreadSafeOccupancyGrid
   {
-    ROS_INFO_STREAM("Launching mission");
-    ROS_INFO_STREAM("map header: "<< _map->info);
-    return false;
-  }
+  private:
+    nav_msgs::OccupancyGrid::ConstPtr _map;  
+    std::mutex _mutex; 
+  public:
+
+   void ActionThreadSafe(std::function<void(nav_msgs::OccupancyGrid::ConstPtr)> Action)
+   {
+     std::lock_guard<std::mutex> lock(_mutex);
+     if (not _map)
+     { 
+       return;
+     }
+
+     return Action(_map);
+   } 
+   void UpdateThreadSafe(nav_msgs::OccupancyGrid::ConstPtr map_to_set) 
+   { 
+     std::lock_guard<std::mutex> lock(_mutex);
+     _map = map_to_set;
+     ROS_INFO_STREAM("Updating map threadsafe");
+   } 
+  };
+
+  ThreadSafeOccupancyGrid _map; ///< Actionlibserver runs in a different thread than callbacks are called on
+  std::condition_variable _new_map_notification; ///< called whenever subscriber receives a new map from gmapping. Will wake action server thread.
 public:
 
   CleaningAction(std::string const& name) :
@@ -42,26 +58,23 @@ public:
     _action_server.start();
   }
 
-  void OnMap(nav_msgs::OccupancyGrid::ConstPtr const& map)
+  void OnMap(nav_msgs::OccupancyGrid::ConstPtr const& new_map)
   { 
-    ROS_INFO("%s: OnMap callback", _action_name.c_str());
-    _map_promise.set_value(map);
+    _map.UpdateThreadSafe(new_map);
+    _new_map_notification.notify_one();
   }
 
-  bool PlanRoute(nav_msgs::OccupancyGrid::ConstPtr map)
-  {
-//   _map_promise = std::promise<nav_msgs::OccupancyGrid::ConstPtr>();
-//    ROS_INFO("%s: Planning route", _action_name.c_str());
-//    vacuumcleaner::cleaningResult result;
-//    result.number_of_tiles_visited = 10;
-//    _action_server.setSucceeded(result);
-    return true;
-  }
-  // This runs on simple actionlib thread
+ // This runs on simple actionlib thread
   void OnGoal(vacuumcleaner::cleaningGoalConstPtr goal)
   {
     _goal = goal;
-    do {} while(true);//PlanRoute(_map_promise.get_future().get()));
+    std::mutex signal_mutex;
+    std::unique_lock<std::mutex> lock(signal_mutex);
+    while(true)
+    {
+       _new_map_notification.wait(lock);
+       _map.ActionThreadSafe([](nav_msgs::OccupancyGrid::ConstPtr Map){ROS_INFO_STREAM("doing threadsafe action");});
+    }
   }
 };
 
