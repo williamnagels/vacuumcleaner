fead42435e414f0d9942158aadf3daf0e7611587
william <william.nagels@gmail.com>
2020-02-20 20:35:26 +0100

    Add subscriber to pull  map transform from tf2

--- src/server.cpp
+++ src/server.cpp
@@ -3,6 +3,7 @@
 #include <actionlib/server/simple_action_server.h>
 #include <vacuumcleaner/cleaningAction.h>
 #include "nav_msgs/OccupancyGrid.h"
+#include <tf2_ros/transform_listener.h>
 class CleaningAction
 {
 private:
@@ -12,12 +13,17 @@ private:
   vacuumcleaner::cleaningFeedback _feedback;
   ros::Subscriber _map_subscriber; ///< Will subscribe to gmappping map updates.
   vacuumcleaner::cleaningGoalConstPtr _goal; ///< received goal from actionlib client
-  nav_msgs::OccupancyGrid::ConstPtr _map;  
+  nav_msgs::OccupancyGrid::ConstPtr _map;
+  uint64_t _map_x = 0;
+  uint64_t _map_y = 0;
+  tf2_ros::Buffer _tf_buffer;
+  tf2_ros::TransformListener _tf_listener;
 public:
 
   CleaningAction(std::string const& name) :
     _action_server(_node_handle, name,false),
-    _action_name(name)
+    _action_name(name),
+    _tf_listener(_tf_buffer)
   {
     std::string map_topic;
     if (not _node_handle.getParam("map_topic", map_topic))
@@ -28,15 +34,31 @@ public:
 
     _map_subscriber = _node_handle.subscribe(map_topic, 1000, &CleaningAction::OnMap, this);
     _action_server.registerGoalCallback([](){ROS_INFO_STREAM("goal callback: "<<std::this_thread::get_id());});
-    //_action_server.registerPreemptCallback(boost::bind(&AveragingAction::preemptCB, this));
-
     _action_server.start();
   }
 
+  int8_t GetXY(nav_msgs::OccupancyGrid::ConstPtr const& map, uint64_t x, uint64_t y)
+  {
+    return map->data[map->info.width * x + y];
+  }
   void OnMap(nav_msgs::OccupancyGrid::ConstPtr const& new_map)
   { 
-    ROS_INFO_STREAM("OnMap: "<<std::this_thread::get_id()); 
     _map = new_map;
+    ROS_INFO_STREAM("OnMap: "<<new_map->info); 
+    ROS_INFO_STREAM("GETXY: "<<GetXY(new_map, 0 , 0));
+    geometry_msgs::TransformStamped transform;
+    try
+    {
+      transform = _tf_buffer.lookupTransform("map", "base_footprint", ros::Time(0));
+      ROS_INFO_STREAM("TRANSFORM: "<< transform); 
+      uint64_t grid_x = (uint64_t)((_map_x - _map->info.origin.position.x) / _map->info.resolution);
+      uint64_t grid_y = (uint64_t)((_map_y - _map->info.origin.position.y) / _map->info.resolution);
+    }
+    catch (tf2::TransformException &ex) 
+    {
+      ROS_WARN("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }
   }
 
   void OnGoal(vacuumcleaner::cleaningGoalConstPtr goal)
