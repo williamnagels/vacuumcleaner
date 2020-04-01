ee7bbb22a9faee8c96274ad5dcfe2f9fc46b20b9
william <william.nagels@gmail.com>
2020-04-01 23:51:31 +0200

    Add move_base client to goal server

--- src/server.cpp
+++ src/server.cpp
@@ -1,9 +1,13 @@
 #include <thread>
 #include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/server/simple_action_server.h>
+#include <actionlib/client/simple_action_client.h>
 #include <vacuumcleaner/cleaningAction.h>
 #include "nav_msgs/OccupancyGrid.h"
 #include <tf2_ros/transform_listener.h>
+
+using MoveBaseClient = actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>;
 class CleaningAction
 {
 private:
@@ -13,7 +17,7 @@ private:
   vacuumcleaner::cleaningFeedback _feedback;
   ros::Subscriber _map_subscriber; ///< Will subscribe to gmappping map updates.
   ros::Timer _timer;  
- 
+  MoveBaseClient _move_base_client;
   vacuumcleaner::cleaningGoalConstPtr _goal; ///< received goal from actionlib client
   nav_msgs::OccupancyGrid::ConstPtr _map;
   double _map_x = 0;
@@ -27,19 +31,20 @@ public:
   CleaningAction(std::string const& name) :
     _action_server(_node_handle, name,false),
     _action_name(name),
-    _tf_listener(_tf_buffer)
+    _tf_listener(_tf_buffer),
+    _move_base_client("move_base", false)
   {
-    std::string map_topic;
+    std::string map_topic("map");
     if (not _node_handle.getParam("map_topic", map_topic))
     {
-      ROS_INFO_STREAM("No map topic provided, using default:\"map\"");
-      map_topic = "map";
+      ROS_INFO_STREAM("No map topic provided, using default:\""<<map_topic<<"\"");
     }
-
+    
+    while(not _move_base_client.waitForServer(ros::Duration(5.0)))
+    {
+      ROS_INFO("Waiting for the move_base action server to come up");
+    }
     _map_subscriber = _node_handle.subscribe(map_topic, 1000, &CleaningAction::OnMap, this);
-    _timer = _node_handle.createTimer(ros::Duration(1.0), [this](ros::TimerEvent const& Timer){
-		    this->OnTick(Timer);
-		    });
     _action_server.registerGoalCallback([](){ROS_INFO_STREAM("goal callback: "<<std::this_thread::get_id());});
     _action_server.start();
   }
@@ -94,39 +99,58 @@ public:
     
     return GetXY(_map, actual_x, actual_y);
   }
+  void OnMoveGoalCompletion(const actionlib::SimpleClientGoalState& state,  const move_base_msgs::MoveBaseResultConstPtr& result) 
+  {
+    //ROS_INFO_STREAM("State: "<< state); 
+    ROS_INFO_STREAM("Result"<< result); 
+  }
   void OnTick(ros::TimerEvent const& Timer)
   {
-    if (not _map)
-    { 
-      return;
-    }
     geometry_msgs::TransformStamped map_transform;
     try
     {
       map_transform = _tf_buffer.lookupTransform("map", "base_footprint", ros::Time(0));
-      ROS_INFO_STREAM("TRANSFORM: "<< map_transform); 
+      //ROS_INFO_STREAM("TRANSFORM: "<< map_transform); 
       _map_x = map_transform.transform.translation.x;
       _map_y = map_transform.transform.translation.y;
-      ROS_INFO_STREAM("map.info.x: "<< _map->info.origin.position.x << " map.info.y: "<< _map->info.origin.position.y); 
-      ROS_INFO_STREAM("_map_x: "<< _map_x << " _map_y: "<< _map_y); 
+      //ROS_INFO_STREAM("map.info.x: "<< _map->info.origin.position.x << " map.info.y: "<< _map->info.origin.position.y); 
+      //ROS_INFO_STREAM("_map_x: "<< _map_x << " _map_y: "<< _map_y); 
+      //
+      //
+      move_base_msgs::MoveBaseGoal goal;
+      //we'll send a goal to the robot to move 1 meter forward
+      goal.target_pose.header.frame_id = "base_footprint";
+      goal.target_pose.header.stamp = ros::Time::now();
+ 
+      goal.target_pose.pose.position.x = 1.0;
+      goal.target_pose.pose.orientation.w = 1.0;
+  
+      ROS_INFO("Sending goal");
+      _move_base_client.sendGoal(goal, [this](auto const& a, auto const& b){this->OnMoveGoalCompletion(a, b);});
     }
     catch (tf2::TransformException &ex) 
     {
       ROS_WARN("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
-    ROS_INFO_STREAM("START");
-    ROS_INFO_STREAM("[" << static_cast<int32_t>(GetRelative(-1, 1)) << ","<<static_cast<int32_t>(GetRelative(0, 1))<< ","<<static_cast<int32_t>(GetRelative(1, 1))<<"]"); 
-    ROS_INFO_STREAM("[" << static_cast<int32_t>(GetRelative(-1, 0)) << ","<<static_cast<int32_t>(GetRelative(0, 0))<< ","<<static_cast<int32_t>(GetRelative(0, 0))<<"]"); 
-    ROS_INFO_STREAM("[" << static_cast<int32_t>(GetRelative(-1, -1)) << ","<<static_cast<int32_t>(GetRelative(0, -1))<< ","<<static_cast<int32_t>(GetRelative(1, -1))<<"]");    
-    ROS_INFO_STREAM("END");
+    //ROS_INFO_STREAM("START");
+    //ROS_INFO_STREAM("[" << static_cast<int32_t>(GetRelative(-1, 1)) << ","<<static_cast<int32_t>(GetRelative(0, 1))<< ","<<static_cast<int32_t>(GetRelative(1, 1))<<"]"); 
+    //ROS_INFO_STREAM("[" << static_cast<int32_t>(GetRelative(-1, 0)) << ","<<static_cast<int32_t>(GetRelative(0, 0))<< ","<<static_cast<int32_t>(GetRelative(0, 0))<<"]"); 
+    //ROS_INFO_STREAM("[" << static_cast<int32_t>(GetRelative(-1, -1)) << ","<<static_cast<int32_t>(GetRelative(0, -1))<< ","<<static_cast<int32_t>(GetRelative(1, -1))<<"]");    
+    //ROS_INFO_STREAM("END");
     //ROS_INFO_STREAM("choppa: "<< static_cast<int32_t>(GetRelative(0,0)));
 
   }
   void OnMap(nav_msgs::OccupancyGrid::ConstPtr const& new_map)
   { 
     _map = new_map;
-    ROS_INFO_STREAM("OnMap: "<<new_map->info); 
+    
+    if (not _timer.isValid())
+    {
+      _timer = _node_handle.createTimer(ros::Duration(1.0), [this](ros::TimerEvent const& Timer){ this->OnTick(Timer); });
+    }
+
+    //ROS_INFO_STREAM("OnMap: "<<new_map->info); 
   }
 
   void OnGoal(vacuumcleaner::cleaningGoalConstPtr goal)
