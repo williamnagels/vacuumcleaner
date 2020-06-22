6cba7b25f18d63c6ed3d7f4fab566b7898cb2174
william <william.nagels@gmail.com>
2020-06-22 20:30:55 +0200

    PI/2 spiral jumps; rotates on each goal point

--- src/route-planning/spiral_planner.cpp
+++ src/route-planning/spiral_planner.cpp
@@ -1,5 +1,5 @@
 #include "spiral_planner.h"
-
+#include <math.h>
 SpiralPlanner::SpiralPlanner(double radius)
     :_radius(radius)
 {
@@ -8,11 +8,12 @@ SpiralPlanner::SpiralPlanner(double radius)
 Coordinates SpiralPlanner::GetNewCoordinates()
 {
     auto UpdatedCoords = Coordinates{
-        (_radius / (double)3.14) * _u * cos(-_u),
-        (_radius / (double)3.14) * _u * sin(-_u)
+        (_radius / M_PI) * _u * cos(-_u),
+        (_radius / M_PI) * _u * sin(-_u)
     };
 
-    _u += 3.14/4;
+    _u += M_PI_2;
+    
 
     return UpdatedCoords;
 }
