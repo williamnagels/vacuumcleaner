58a22323869a3466f2354d17d0d403e5143f0017
william <william.nagels@gmail.com>
2020-06-28 00:22:06 +0200

    remove exploration files; use yaml file to configure cleaning

--- src/server.cpp
+++ src/server.cpp
@@ -38,8 +38,8 @@ public:
     :_action_server(_node_handle, name,false)
     ,_action_name(name)
     ,_robot_radius_in_meter(GetParameter(_node_handle, "robot_radius", 0.1))
-    ,_map(_node_handle, GetParameter(_node_handle, "map_topic", std::string("map")))
-    ,_spiral_planner(_robot_radius_in_meter, M_PI/GetParameter(_node_handle, "spiral_delta_denominator", 8))
+    ,_map(_node_handle, GetParameter(_node_handle, "/server/vacuumcleaner/map_topic", std::string("map")))
+    ,_spiral_planner(_robot_radius_in_meter, M_PI/GetParameter(_node_handle, "/server/vacuumcleaner/spiral_delta_denominator", 8))
     ,_movement(_spiral_planner)
   {
     _action_server.registerGoalCallback(std::bind(std::mem_fn(&CleaningAction::OnGoal), this));
